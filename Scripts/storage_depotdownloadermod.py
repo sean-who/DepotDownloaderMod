6e53aaeb5bb517264fbf8247afbacfbdaf8c63df
oureveryday <oureveryday@163.com>
2025-01-04 20:59:52 +0800

    Update script

--- Scripts/storage_depotdownloadermod.py
+++ Scripts/storage_depotdownloadermod.py
@@ -1,142 +1,400 @@
+import sys
 import os
-import vdf
-import time
-import shutil
-import winreg
-import sqlite3
-import argparse
-import requests
 import traceback
+import time
+import logging
+import subprocess
+import asyncio
+import aiofiles
+import colorlog
+import httpx
+import winreg
+import ujson as json
+import vdf
+import base64
+import zlib
+from typing import Any
 from pathlib import Path
-from multiprocessing.pool import ThreadPool
-from multiprocessing.dummy import Pool, Lock
+from colorama import init, Fore, Back, Style
 
-lock = Lock()
+init()
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-depotdownloader = "DepotDownloadermod.exe"
-depotdownlaoderargs = "-max-servers 128 -max-downloads 256 -verify-all"
+lock = asyncio.Lock()
+client = httpx.AsyncClient(trust_env=True)
 
-def get(sha, path):
-    url_list = [f'https://cdn.jsdelivr.net/gh/{repo}@{sha}/{path}',
-                f'https://ghproxy.com/https://raw.githubusercontent.com/{repo}/{sha}/{path}']
+DEPOTDOWNLOADER = "DepotDownloadermod.exe"
+DEPOTDOWNLOADER_ARGS = "-max-servers 128 -max-downloads 256 -verify-all"
+
+Pro = False
+
+DEFAULT_CONFIG = {
+    "Github_Personal_Token": "",
+    "Custom_Steam_Path": "",
+    "QA1": "温馨提示: Github_Personal_Token可在Github设置的最底下开发者选项找到, 详情看教程",
+    "教程": "https://ikunshare.com/Onekey_tutorial"
+}
+
+LOG_FORMAT = '%(log_color)s%(message)s'
+LOG_COLORS = {
+    'INFO': 'cyan',
+    'WARNING': 'yellow',
+    'ERROR': 'red',
+    'CRITICAL': 'purple',
+}
+
+
+def init_log(level=logging.DEBUG) -> logging.Logger:
+    """ 初始化日志模块 """
+    logger = logging.getLogger('Onekey')
+    logger.setLevel(level)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+
+    fmt = colorlog.ColoredFormatter(LOG_FORMAT, log_colors=LOG_COLORS)
+    stream_handler.setFormatter(fmt)
+
+    if not logger.handlers:
+        logger.addHandler(stream_handler)
+
+    return logger
+
+
+log = init_log()
+
+
+def init():
+    """ 输出初始化信息 """
+    banner_lines = [
+        "  _____   __   _   _____   _   _    _____  __    __ ",
+        " /  _  \\ |  \\ | | | ____| | | / /  | ____| \\ \\  / /",
+        " | | | | |   \\| | | |__   | |/ /   | |__    \\ \\/ /",
+        " | | | | | |\\   | |  __|  | |\\ \\   |  __|    \\  / ",
+        " | |_| | | | \\  | | |___  | | \\ \\  | |___    / /",
+        " \\_____/ |_|  \\_| |_____| |_|  \\_\\ |_____|  /_/",
+    ]
+    for line in banner_lines:
+        log.info(line)
+
+    log.info('DepotDownloadermod下载脚本一键生成工具')
+    log.info('作者: ikun0014 修改: oureveryday')
+    log.warning('本项目采用GNU General Public License v3开源许可证, 请勿用于商业用途')
+    log.info('版本: 1.3.5')
+    log.info(
+        '项目Github仓库: https://github.com/ikunshare/Onekey \n Gitee: https://gitee.com/ikun0014/Onekey'
+    )
+    log.info('官网: ikunshare.com')
+    log.warning(
+        '本项目完全开源免费, 如果你在淘宝, QQ群内通过购买方式获得, 赶紧回去骂商家死全家\n   交流群组:\n    https://t.me/ikunshare_qun'
+    )
+    log.info('App ID可以在SteamDB, SteamUI或Steam商店链接页面查看')
+
+
+def stack_error(exception: Exception) -> str:
+    """ 处理错误堆栈 """
+    stack_trace = traceback.format_exception(
+        type(exception), exception, exception.__traceback__)
+    return ''.join(stack_trace)
+
+
+async def gen_config_file():
+    """ 生成配置文件 """
+    try:
+        async with aiofiles.open("./config.json", mode="w", encoding="utf-8") as f:
+            await f.write(json.dumps(DEFAULT_CONFIG, indent=2, ensure_ascii=False, escape_forward_slashes=False))
+
+        log.info('程序可能为第一次启动或配置重置,请填写配置文件后重新启动程序')
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except Exception as e:
+        log.error(f'配置文件生成失败,{stack_error(e)}')
+
+
+async def load_config():
+    """ 加载配置文件 """
+    if not os.path.exists('./config.json'):
+        await gen_config_file()
+        os.system('pause')
+        sys.exit()
+
+    try:
+        async with aiofiles.open("./config.json", mode="r", encoding="utf-8") as f:
+            config = json.loads(await f.read())
+            return config
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except Exception as e:
+        log.error(f"配置文件加载失败，原因: {stack_error(e)},重置配置文件中...")
+        os.remove("./config.json")
+        await gen_config_file()
+        os.system('pause')
+        sys.exit()
+
+config = asyncio.run(load_config())
+
+
+async def check_github_api_rate_limit(headers):
+    """ 检查Github请求数 """
+
+    if headers != None:
+        log.info(f"您已配置Github Token")
+
+    url = 'https://api.github.com/rate_limit'
+    try:
+        r = await client.get(url, headers=headers)
+        r_json = r.json()
+        if r.status_code == 200:
+            rate_limit = r_json.get('rate', {})
+            remaining_requests = rate_limit.get('remaining', 0)
+            reset_time = rate_limit.get('reset', 0)
+            reset_time_formatted = time.strftime(
+                '%Y-%m-%d %H:%M:%S', time.localtime(reset_time))
+            log.info(f'剩余请求次数: {remaining_requests}')
+            if remaining_requests == 0:
+                log.warning(f'GitHub API 请求数已用尽, 将在 {reset_time_formatted} 重置,建议生成一个填在配置文件里')
+        else:
+            log.error('Github请求数检查失败, 网络错误')
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except httpx.ConnectError as e:
+        log.error(f'检查Github API 请求数失败, {stack_error(e)}')
+    except httpx.ConnectTimeout as e:
+        log.error(f'检查Github API 请求数超时: {stack_error(e)}')
+    except Exception as e:
+        log.error(f'发生错误: {stack_error(e)}')
+
+
+async def checkcn() -> bool:
+    try:
+        req = await client.get('https://mips.kugou.com/check/iscn?&format=json')
+        body = req.json()
+        scn = bool(body['flag'])
+        if (not scn):
+            log.info(
+                f"您在非中国大陆地区({body['country']})上使用了项目, 已自动切换回Github官方下载CDN")
+            os.environ['IS_CN'] = 'no'
+            return False
+        else:
+            os.environ['IS_CN'] = 'yes'
+            return True
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except httpx.ConnectError as e:
+        os.environ['IS_CN'] = 'yes'
+        log.warning('检查服务器位置失败，已忽略，自动认为你在中国大陆')
+        log.warning(stack_error(e))
+        return False
+    
+def csharp_gzip(b64_string):
+    # Base64 解码
+    compressed_data = base64.b64decode(b64_string)
+    
+
+    if len(compressed_data) <= 18:
+        raise ValueError("Data too short to be gzip format")
+        
+    decompressor = zlib.decompressobj(-zlib.MAX_WBITS)
+    # skip gz header
+    decompressed_data = decompressor.decompress(compressed_data[10:])
+    decompressed_data += decompressor.flush()
+    
+    return decompressed_data.decode('utf-8')
+
+async def get(sha: str, path: str, repo: str):
+    if os.environ.get('IS_CN') == 'yes':
+        url_list = [
+            f'https://jsdelivr.pai233.top/gh/{repo}@{sha}/{path}',
+            f'https://cdn.jsdmirror.com/gh/{repo}@{sha}/{path}',
+            f'https://raw.gitmirror.com/{repo}/{sha}/{path}',
+            f'https://raw.dgithub.xyz/{repo}/{sha}/{path}',
+            f'https://gh.akass.cn/{repo}/{sha}/{path}'
+        ]
+    else:
+        url_list = [
+            f'https://raw.githubusercontent.com/{repo}/{sha}/{path}'
+        ]
     retry = 3
-    while True:
+    while retry > 0:
         for url in url_list:
             try:
-                r = requests.get(url)
+                r = await client.get(url, timeout=30)
                 if r.status_code == 200:
-                    return r.content
-            except requests.exceptions.ConnectionError:
-                print(f'获取失败: {path}')
-                retry -= 1
-                if not retry:
-                    print(f'超过最大重试次数: {path}')
-                    raise
+                    if Pro:
+                        return csharp_gzip(r.text)
+                    else:
+                        return r.read()
+                else:
+                    log.error(f'获取失败: {path} - 状态码: {r.status_code}')
+            except KeyboardInterrupt:
+                log.info("程序已退出")
+            except httpx.ConnectError as e:
+                log.error(f'获取失败: {path} - 连接错误: {str(e)}')
+            except httpx.ConnectTimeout as e:
+                log.error(f'连接超时: {url} - 错误: {str(e)}')
 
-def downloader_add(appid,path: Path,outpath):
-    depotid = path[0:path.find('_')]
-    manifestid = path[path.find('_') + 1:path.find('.')]
-    out = Path(os.path.join(outpath, f'{appid}.bat'))
-    with out.open('a') as f:
-        f.write(f'{depotdownloader} -app {appid} -depot {depotid} -manifest {manifestid} -manifestfile {path} -depotkeys {appid}.key {depotdownlaoderargs}\n')
-    return True
+        retry -= 1
+        log.warning(f'重试剩余次数: {retry} - {path}')
+
+    log.error(f'超过最大重试次数: {path}')
+    raise Exception(f'无法下载: {path}')
 
 
-def get_manifest(sha, path, output_path: Path, app_id=None):
+async def get_manifest(app_id: str, sha: str, path: str, repo: str) -> list:
+    collected_depots = []
+    depot_cache_path = Path(os.getcwd())
     try:
         if path.endswith('.manifest'):
-            save_path = Path(os.path.join(output_path,path))
-            content = get(sha, path)
-            with lock:
-                print(f'清单下载成功: {path}')
-            with save_path.open('wb') as f:
-                f.write(content)
-            downloader_add(app_id,path,output_path)
-
-        elif path == 'config.vdf':
-            content = get(sha, path)
-            with lock:
-                print(f'密钥下载成功: {path}')
-            depots_config = vdf.loads(content.decode(encoding='utf-8'))
-
+            save_path = depot_cache_path / path
+            if save_path.exists():
+                log.warning(f'已存在清单: {save_path}')
+                return collected_depots
+            content = await get(sha, path, repo)
+            log.info(f'清单下载成功: {path}')
+            async with aiofiles.open(save_path, 'wb') as f:
+                await f.write(content)
         elif path == 'Key.vdf':
-            content = get(sha, path)
-            with lock:
-                print(f'密钥下载成功: {path}')
-            depots_config = vdf.loads(content.decode(encoding='utf-8'))
-
-            if depotkey_add(
-                    [(depot_id, '1', depots_config['depots'][depot_id]['DecryptionKey'])
-                     for depot_id in depots_config['depots']],output_path,app_id):
-                print('导入depotdownloader成功')
+            content = await get(sha, path, repo)
+            log.info(f'密钥下载成功: {path}')
+            depots_config = vdf.loads(content.decode('utf-8'))
+            if depots_config:
+                async with aiofiles.open(depot_cache_path / f"{app_id}.key", 'w', encoding="utf-8") as f:
+                    for depot_id, depot_info in depots_config['depots'].items():
+                        await f.write(f'{depot_id};{depot_info["DecryptionKey"]}\n')
     except KeyboardInterrupt:
+        log.info("程序已退出")
+    except Exception as e:
+        log.error(f'处理失败: {path} - {stack_error(e)}')
         raise
-    except:
-        traceback.print_exc()
-        raise
-    return True
+    return collected_depots
+    
+async def depotdownloadermod_add(app_id: str, manifests: list) -> bool:
+    async with lock:
+        log.info(f'DepotDownloader 下载文件生成: {app_id}.bat')
+        try:
+            async with aiofiles.open(f'{app_id}.bat', mode="w", encoding="utf-8") as bat_file:
+                for manifest in manifests:
+                    depot_id = manifest[0:manifest.find('_')]
+                    manifest_id = manifest[manifest.find('_') + 1:manifest.find('.')]
+                    await bat_file.write(f'{DEPOTDOWNLOADER} -app {app_id} -depot {depot_id} -manifest {manifest_id} -manifestfile {manifest} -depotkeys {app_id}.key {DEPOTDOWNLOADER_ARGS}\n')
+        except Exception as e:
+            log.error(f'出现错误: {e}')
+            return False
 
-def depotkey_add(depot_list,Outpath: Path,app_id):
-    for depot_id, type_, depot_key in depot_list:
-        if depot_key:
-            depot_key = f'{depot_key}'
-        out = Path(os.path.join(Outpath, f'{app_id}.key'))
-        with out.open('a') as f:
-            f.write(f'{depot_id};{depot_key}\n')
-        
-    return True
+async def fetch_branch_info(url, headers) -> str | None:
+    try:
+        r = await client.get(url, headers=headers)
+        return r.json()
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except Exception as e:
+        log.error(f'获取信息失败: {stack_error(e)}')
+        return None
+    except httpx.ConnectTimeout as e:
+        log.error(f'获取信息时超时: {stack_error(e)}')
+        return None
+    
+async def get_pro_token():
+    try:
+        r = await client.get("https://gitee.com/pjy612/sai/raw/master/free")
+        return csharp_gzip(r.text)
+    except KeyboardInterrupt:
+        log.info("程序已退出")
+    except Exception as e:
+        log.error(f'获取信息失败: {stack_error(e)}')
+        return None
+    except httpx.ConnectTimeout as e:
+        log.error(f'获取信息时超时: {stack_error(e)}')
+        return None
+
+async def get_latest_repo_info(repos: list, app_id: str, headers) -> Any | None:
+    latest_date = None
+    selected_repo = None
+    for repo in repos:
+        url = f'https://api.github.com/repos/{repo}/branches/{app_id}'
+        r_json = await fetch_branch_info(url, headers)
+        if r_json and 'commit' in r_json:
+            date = r_json['commit']['commit']['author']['date']
+            if (latest_date is None) or (date > latest_date):
+                latest_date = date
+                selected_repo = repo
+
+    return selected_repo, latest_date
 
 
-def get_script_path():
-    return os.getcwd()
-
-
-def main(app_id,path=get_script_path()):
-    Outpath = Path(path)
-    url = f'https://api.github.com/repos/{repo}/branches/{app_id}'
-    r = requests.get(url)
-    if 'commit' in r.json():
-        sha = r.json()['commit']['sha']
-        url = r.json()['commit']['commit']['tree']['url']
-        r = requests.get(url)
-        if 'tree' in r.json():
-            result_list = []
-            with Pool(32) as pool:
-                pool: ThreadPool
-                for i in r.json()['tree']:
-                    result_list.append(pool.apply_async(get_manifest, (sha, i['path'], Outpath, app_id)))
-                try:
-                    while pool._state == 'RUN':
-                        if all([result.ready() for result in result_list]):
-                            break
-                        time.sleep(0.1)
-                except KeyboardInterrupt:
-                    with lock:
-                        pool.terminate()
-                    raise
-            if all([result.successful() for result in result_list]):
-                print(f'导入成功: {app_id}')
+async def main(app_id: str, repos: list) -> bool:
+    app_id_list = list(filter(str.isdecimal, app_id.strip().split('-')))
+    if not app_id_list:
+        log.error(f'App ID无效')
+        return False
+    app_id = app_id_list[0]
+    github_token = config.get("Github_Personal_Token", "")
+    if repos.__len__() == 1 and repos[0] == 'P-ToyStore/SteamManifestCache_Pro':
+        github_token = get_pro_token()
+        global Pro
+        Pro = True
+        headers = {'User-Agent':'SteamDepotDownload','Authorization': f'Bearer {github_token}'}
+    else:
+        headers = {'Authorization': f'Bearer {github_token}'} if github_token else None
+    await checkcn()
+    await check_github_api_rate_limit(headers)
+    selected_repo, latest_date = await get_latest_repo_info(repos, app_id, headers)
+    if (selected_repo):
+        log.info(f'选择清单仓库: {selected_repo}')
+        url = f'https://api.github.com/repos/{selected_repo}/branches/{app_id}'
+        r_json = await fetch_branch_info(url, headers)
+        if (r_json) and ('commit' in r_json):
+            sha = r_json['commit']['sha']
+            url = r_json['commit']['commit']['tree']['url']
+            r2_json = await fetch_branch_info(url, headers)
+            if (r2_json) and ('tree' in r2_json):
+                manifests = [item['path'] for item in r2_json['tree'] if item['path'].endswith('.manifest')]
+                for item in r2_json['tree']:
+                    await get_manifest(app_id, sha, item['path'], selected_repo)
+                await depotdownloadermod_add(app_id, manifests)
+                log.info('已添加下载文件')
+                log.info(f'清单最后更新时间: {latest_date}')
+                log.info(f'导入成功: {app_id}')
+                await client.aclose()
+                os.system('pause')
                 return True
-    print(f'导入失败: {app_id}')
+    log.error(f'清单下载或生成失败: {app_id}')
+    await client.aclose()
+    os.system('pause')
     return False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--repo', default='ManifestHub/ManifestHub')
-parser.add_argument('-a', '--app-id')
-parser.add_argument('-p', '--output-path')
-args = parser.parse_args()
-repo = args.repo
+def select_repo(repos):
+    print(f"\n{Fore.YELLOW}{Back.BLACK}{Style.BRIGHT}请选择要使用的存储库：{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}1. 全部仓库{Style.RESET_ALL}")
+    for i, repo in enumerate(repos, 2):
+        print(f"{Fore.GREEN}{i}. {repo}{Style.RESET_ALL}")
+    
+    while True:
+        try:
+            choice = int(input(f"\n{Fore.CYAN}请输入数字选择: {Style.RESET_ALL}"))
+            if 1 <= choice <= len(repos) + 1:
+                if choice == 1:
+                    return repos
+                else:
+                    return [repos[choice-2]]
+            else:
+                print(f"{Fore.RED}无效的选择，请重试{Style.RESET_ALL}")
+        except ValueError:
+            print(f"{Fore.RED}请输入有效的数字{Style.RESET_ALL}")
+
 if __name__ == '__main__':
+    init()
     try:
-        if args.output_path:
-            if not os.path.exists(args.output_path):   
-                os.makedirs(args.output_path) 
-            main(args.app_id or input('appid: '),args.output_path)
-        else:
-            main(args.app_id or input('appid: '))
+        repos = [
+            'ikun0014/ManifestHub',
+            'Auiowu/ManifestAutoUpdate',
+            'tymolu233/ManifestAutoUpdate',
+#            'P-ToyStore/SteamManifestCache_Pro'
+        ]
+        app_id = input(f"{Fore.CYAN}{Back.BLACK}{Style.BRIGHT}请输入游戏AppID: {Style.RESET_ALL}").strip()
+        selected_repos = select_repo(repos)
+        asyncio.run(main(app_id, repos))
     except KeyboardInterrupt:
-        exit()
-    except:
-        traceback.print_exc()
-    if not args.app_id and not args.output_path:
-        os.system('pause')
+        log.info("程序已退出")
+    except SystemExit:
+        sys.exit()
